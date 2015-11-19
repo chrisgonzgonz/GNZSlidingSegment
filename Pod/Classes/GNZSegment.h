abe029a0c1aa94f44284f3b9b0dd2eea1830dc0d
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-11-19 16:23:39 -0500

    update example project

---
+++ Pod/Classes/GNZSegment.h
@@ -0,0 +1,13 @@
+//
+//  GNZSegment.h
+//  Pods
+//
+//  Created by Chris Gonzales on 11/19/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GNZSegment <NSObject>
+
+@end
---
+++ Pod/Classes/UISegmentedControl+GNZCompatibility.m
@@ -0,0 +1,13 @@
+//
+//  UISegmentedControl+PageCompatibility.m
+//  Pods
+//
+//  Created by Chris Gonzales on 11/19/15.
+//
+//
+
+#import "UISegmentedControl+PageCompatibility.h"
+
+@implementation UISegmentedControl (PageCompatibility)
+
+@end
